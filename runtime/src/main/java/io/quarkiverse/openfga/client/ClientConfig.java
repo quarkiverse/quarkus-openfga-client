40d2dadd73703632fae1d943cbf9ee18df860c76
Kevin Wooten <kevin@wooten.com>
2026-02-13 07:48:28 -0700

    Add missing nullability annotations

--- runtime/src/main/java/io/quarkiverse/openfga/client/ClientConfig.java
+++ runtime/src/main/java/io/quarkiverse/openfga/client/ClientConfig.java
@@ -1,10 +1,12 @@
 package io.quarkiverse.openfga.client;
 
+import javax.annotation.Nullable;
+
 public final class ClientConfig {
     private final String storeId;
     private final String authorizationModelId;
 
-    public ClientConfig(String storeId, String authorizationModelId) {
+    public ClientConfig(String storeId, @Nullable String authorizationModelId) {
         this.storeId = storeId;
         this.authorizationModelId = authorizationModelId;
     }
